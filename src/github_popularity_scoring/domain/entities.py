aadd40b99299fe806c7a6aae193424c85007ae48
bvi <bvi@apladis.com>
2026-04-14 14:45:10 +0300

    feat(service): add scafold for searching repositories capability

--- src/github_popularity_scoring/domain/entities.py
+++ src/github_popularity_scoring/domain/entities.py
@@ -1,12 +1,27 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from datetime import datetime
+from datetime import date, datetime
 
 
 @dataclass
 class Repository:
     name: str
+    language: str
     updated_at: datetime
     stars: int
     forks: int
+
+@dataclass
+class RepositorySearchCriteria:
+    language: str
+    created_after: date
+    limit: int = 10
+
+    def __post_init__(self):
+        object.__setattr__(self,'language',self.language.strip())
+
+@dataclass
+class ScoredRepository:
+    repository: Repository
+    popularity_score: float
---
+++ src/github_popularity_scoring/service/repositories.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+
+from datetime import date
+from typing import Protocol
+
+from github_popularity_scoring.domain.entities import Repository, RepositorySearchCriteria, ScoredRepository
+from github_popularity_scoring.domain.scoring import PopularityScorer
+
+
+class RepositorySearchPort(Protocol):
+    """
+    A contract for searching repositories.
+    It inverts the use case dependency on GitHub-specific search logic.
+    """
+    async def search_repositories(self, criteria: RepositorySearchCriteria) -> list[Repository]:
+        """
+        Return repositories matching the provided criteria.
+
+        :param criteria: repository search criteria
+        :return list[Repository]: list of repositories matching the provided criteria.
+        """
+        ...
+
+class SearchRepositoriesUseCase:
+
+    _repository_search: RepositorySearchPort
+    _scorer: PopularityScorer
+
+    def __init__(self, repository_search: RepositorySearchPort, scorer: PopularityScorer) -> None:
+        self._repository_search = repository_search
+        self._scorer = scorer
+
+    async def execute(self, criteria: RepositorySearchCriteria) -> list[ScoredRepository]:
+        language = criteria.language
+
+        if not language:
+            raise ValueError("Language not provided")
+        if criteria.created_after > date.today():
+            raise ValueError("created_after must not be in future")
+        if criteria.limit < 1:
+            raise ValueError("Limit must be greater than 0")
+
+        repositories = await self._repository_search.search_repositories(criteria)
+
+        scored_repositories = [
+            ScoredRepository(
+                repository=repo,
+                popularity_score=self._scorer.score(repo)
+            ) for repo in repositories
+        ]
+
+        return sorted(scored_repositories, key=lambda repo: repo.popularity_score, reverse=True)[:criteria.limit]
